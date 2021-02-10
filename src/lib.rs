651a62ff3784a313dea965a0fb143c1f9b2df3eb
demont93 <demont93@gmail.com>
2021-02-10 03:31:27 -0400

    Fixed visibility.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ pub use image_buffer::ImageBuffer;
 pub use cpixel::Cpixel;
 pub use dimensions::{Dim, Dimensions};
 
+pub mod converter;
+pub mod pixel;
+
 mod yuv;
 mod image_buffer;
 mod dimensions;
 mod cpixel;
-mod converter;
-mod pixel;
