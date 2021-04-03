9c8bec8ec59205dac47b56b92fc59e833ab0bf78
demont93 <demont93@gmail.com>
2021-04-03 00:31:10 -0400

    Removed android.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-#[cfg(target_os = "android")]
-pub mod android;
-
 mod converter;
 mod dimensions;
 mod pixel;
 mod buffer_2d;
 mod yuv;
-
-#[cfg(target_os = "android")]
-pub use android::*;
 
 pub use converter::Converter;
 
